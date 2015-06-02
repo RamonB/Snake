e6449d39e58fafbbc2ca4da41f0066289739da6b
Ramon Boss <Ramon.Boss@Swisscom.com>
2015-06-02 15:21:33 +0200

    added constructor

--- src/Figure/Snake.java
+++ src/Figure/Snake.java
@@ -1,9 +1,15 @@
 package Figure;
 
+import java.awt.Point;
+
 /**
  *
  * @author ramon
  */
 public class Snake extends AbstractFigure {
+
+  public Snake(Point position) {
+    super(position);
+  }
   
 }
