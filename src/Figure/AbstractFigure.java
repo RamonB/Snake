72372aa406d5d69f3f2ecf0d40a652de21036bdf
Ramon Boss <Ramon.Boss@Swisscom.com>
2015-06-02 14:47:06 +0200

    added position an move methods

--- src/Figure/AbstractFigure.java
+++ src/Figure/AbstractFigure.java
@@ -1,9 +1,33 @@
 package Figure;
 
+import java.awt.Point;
+
 /**
  *
  * @author ramon
  */
 abstract class AbstractFigure {
+  /**
+   * Current position of a figure
+   */
+  private Point position;
+  
+  /**
+   * Moves the figure horizonal
+   * 
+   * @param step Step to move
+   */
+  public void moveX(int step) {
+    this.position.x += step;
+  }
+  
+  /**
+   * Moves the figure vertical
+   * 
+   * @param step Step to move
+   */
+  public void moveY(int step) {
+    this.position.y += step;
+  }
   
 }
