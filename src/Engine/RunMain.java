12708e3a122ead93fcce77a643b42bc0d505a987
Ramon Boss <Ramon.Boss@Swisscom.com>
2015-06-02 15:10:04 +0200

    added main class

---
+++ src/Engine/RunMain.java
@@ -0,0 +1,16 @@
+package Engine;
+
+/**
+ *
+ * @author ramon
+ */
+public class RunMain {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    
+  }
+  
+}
